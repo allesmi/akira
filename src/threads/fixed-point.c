5a73ce47484bea915453ea50c6bcd4829ae628db
Alexander Miller <alles.mil@gmail.com>
2014-10-24 18:57:33 +0200

    Optics

--- src/threads/fixed-point.c
+++ src/threads/fixed-point.c
@@ -24,26 +24,32 @@ fixed_point subtract(fixed_point x, fixed_point y)
 {
 	return 0;
 }
+
 fixed_point add_int(fixed_point x, int n)
 {
 	return 0;
 }
+
 fixed_point sub_int(fixed_point x, int n)
 {
 	return 0;
 }
+
 fixed_point multi(fixed_point x, fixed_point y)
 {
 	return 0;
 }
+
 fixed_point multi_int(fixed_point x, int n)
 {
 	return 0;
 }
+
 fixed_point div(fixed_point x, fixed_point y)
 {
 	return 0;
 }
+
 fixed_point div_int(fixed_point x, int n)
 {
 	return 0;
