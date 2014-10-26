1697c332112e1ecbe4408fce8e56770bdc5782d7
liquidsunset <dbrand@cosy.sbg.ac.at>
2014-10-26 17:25:07 +0100

    akira is calculator

--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -1,6 +1,8 @@
 #ifndef FIXED_POINT_H
 #define FIXED_POINT_H
 
+#define EXPONENT 14
+
 typedef int fixed_point;
 
 fixed_point to_fixed_point(int n);
@@ -16,6 +18,7 @@ fixed_point div(fixed_point x, fixed_point y);
 fixed_point div_int(fixed_point x, int n);
 fixed_point max(fixed_point x, fixed_point y);
 fixed_point min(fixed_point x, fixed_point y);
+int power(int base, int exponent);
 
 
 #endif
