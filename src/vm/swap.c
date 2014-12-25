221200056b997427085d0d04b2c5bf49b3a742be
Alexander Miller <alles.mil@gmail.com>
2014-12-25 14:39:04 +0100

    Added swap init to boot process, added code for storing and retrieving pages on swap disk

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -1,19 +1,87 @@
+#include <inttypes.h>
+#include <debug.h>
+#include <stdio.h>
+
 #include "vm/swap.h"
+#include "devices/block.h"
+#include "threads/palloc.h"
+
+static struct block * swap_device;
+
+static block_sector_t free_list;
+static block_sector_t unused;
+
+struct free_slot
+{
+	block_sector_t next;
+};
 
 void
 swap_init(void)
 {
+	swap_device = block_get_role(BLOCK_SWAP);
 
+	free_list = 0;
+	unused = 0;
+
+	// // A small test program
+	// char * a = "First slot";
+	// char * b = "Second slot";
+	// char * c = "Third slot";
+	// char * d = "Fourth slot";
+
+	// block_sector_t s1 = swap_store(a);
+	// block_sector_t s2 = swap_store(b);
+	// block_sector_t s3 = swap_store(c);
+
+	// char * bo = swap_retrieve(s2);
+	// printf("Retrieved '%s'\n", bo);
+
+	// swap_store(d);
+
+	// int i;
+	// for(i = 0; i < 3; i++)
+	// {
+	// 	char * o = swap_retrieve((block_sector_t)i);
+	// 	printf("Retrieved from slot %d: '%s'.\n", i, o);
+	// }
 }
 
-unsigned
+block_sector_t
 swap_store(void * page)
 {
+	if (free_list == unused && unused == block_size(swap_device))
+		PANIC ("Swap is full!");
+	
+	block_sector_t f = free_list;
 
+	void * p = palloc_get_page(0);
+	block_read(swap_device, f, p);
+	if(free_list != unused)
+		free_list = ((struct free_slot *)p)->next;
+	else
+	{
+		unused = unused + 1;
+		free_list = unused;
+	}
+
+	block_write(swap_device, f, page);
+
+	return f;
 }
 
 void *
-swap_retrieve(unsigned slot_no)
+swap_retrieve(block_sector_t slot_no)
 {
-	
+	void * p = palloc_get_page(PAL_USER | PAL_ZERO);
+
+	block_read(swap_device, slot_no, p);
+
+	struct free_slot f;
+	f.next = free_list;
+
+	block_write(swap_device, slot_no, &f);
+	free_list = slot_no;
+
+	return p;
 }
--- src/vm/swap.h
+++ src/vm/swap.h
@@ -1,8 +1,10 @@
 #ifndef VM_SWAP_H
 #define VM_SWAP_H
 
+#include <devices/block.h>
+
 void swap_init(void);
-unsigned swap_store(void*);
-void * swap_retrieve(unsigned);
+block_sector_t swap_store(void*);
+void * swap_retrieve(block_sector_t);
 
 #endif
