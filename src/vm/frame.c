83dc462e1036926cd533cb767b6c243b413b9d06
Alexander Miller <alles.mil@gmail.com>
2014-12-26 18:36:58 +0100

    Reverting frame table to stub

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -12,45 +12,23 @@
 static void evict_next_frame(void);
 static bool frames_available(void);
 
-struct bitmap * frames_bm;
-
 void
 frame_init(void)
 {
-	// Assumption: When init_ram_pages is the number of pages
-	// in physical memory, init_ram_pages/2 is the number available
-	// for user programs
-	frames_bm = bitmap_create(init_ram_pages / 2);
+
 }
 
 void *
 frame_alloc(void)
 {
-	if (!frames_available())
-	{
-		evict_next_frame();
-	}
-
 	void * page = palloc_get_page(PAL_USER);
-	uintptr_t phys_address = vtop(page);
-
-	size_t index = (size_t)(phys_address/PGSIZE - init_ram_pages/2);
-
-	bitmap_set(frames_bm, index, true);
-
-	// TODO: Set pointer of frame_entry at PHYS_ADDRESS/PGSIZE to
-	// PAGE.
-
+	
 	return page;
 }
 
 void
 frame_free(void * page)
 {
-	// TODO: delete entry in frame table
-	size_t index = (size_t)page/PGSIZE;
-	bitmap_set(frames_bm, index, false);
-
 	palloc_free_page(page);
 }
 
@@ -63,5 +41,5 @@ evict_next_frame(void)
 static bool
 frames_available(void)
 {
-	return !bitmap_all(frames_bm, 0, init_ram_pages/2);
+	true;
 }
