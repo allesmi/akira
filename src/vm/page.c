1157db97ee53b2ed976971c07d44077502d3fe1b
Alexander Miller <alles.mil@gmail.com>
2014-12-28 19:03:22 +0100

    changed page_destroy function

--- src/vm/page.c
+++ src/vm/page.c
@@ -38,7 +38,6 @@ mmfile_add_to_page_table (struct file * file, int ofs, int size, void * addr, si
 	free(pte);
 
 	return false;
-
 }
 
 bool
@@ -48,14 +47,6 @@ page_add_entry(struct page * p)
 		printf("Added to page table: %p+%d\n", p->vaddr, p->size);
 	struct thread * t = thread_current();
 	return hash_insert(&t->pages, &p->h_elem) == NULL;
-}
-
-void
-page_delete_entry (struct page * p)
-{
-	struct thread * t = thread_current();
-	hash_delete (&t->pages, &p->h_elem);
-	free(p);
 }
 
 struct page *
@@ -93,10 +84,15 @@ page_less(const struct hash_elem *a, const struct hash_elem *b, void *aux UNUSED
 }
 
 void
-page_destroy(struct hash_elem *e, void* aux UNUSED)
+page_hash_destroy(struct hash_elem *e, void* aux UNUSED)
 {
 	struct page * p = hash_entry(e, struct page, h_elem);
+	page_destroy(p);
+}
 
+void
+page_destroy(struct page * p)
+{
 	// TODO: Write back to file
 
 	if(debug)
